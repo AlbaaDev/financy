8e9f0c0f29193087785a7b64975e27461c7b3d05
AlbaaDev <abi-boys@live.fr>
2026-01-01 23:58:04 +0100

    init

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './components/header/Header.jsx';
+import SideBare from './components/sideBare/SideBare.jsx';
+
+function App() {
+    return (
+        <>
+            <SideBare />
+            <Header />
+        </>
+    );
+};
+
+export default App;
---
+++ front/src/components/sideBare/SideBare.jsx
@@ -0,0 +1,13 @@
+export default function SideBare() {
+    return (
+        <lu>
+            <li>Dashboard</li>
+            <li>Transactions</li>
+            <li>Comptes</li>
+            <li>Cartes de crédit</li>
+            <li>Budgets</li>
+            <li>Calendrier</li>
+            <li>Paramètre</li>
+        </lu>            
+    );
+}
